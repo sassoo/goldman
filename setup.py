5b72dfb329854e86bc7ae1bc2b4c8735510daf34
sassoo <scottmcclanahan2002@gmail.com>
2015-12-28 19:54:29 -0500

    pep8 setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,19 @@
+""" PyPI setup.cfg """
+
 from setuptools import setup
 
 setup(
-    name = 'goldman',
-    packages = ['goldman'],
-    version = '0.1',
-    description = 'An opinionated WSGI web framework',
-    author = 'Sassoo',
+    name='goldman',
+    packages=['goldman'],
+    version='0.1',
+    description='An opinionated WSGI web framework',
+    author='Sassoo',
     author_email='noreply@devnull.seriously',
-    url = 'https://github.com/sassoo/goldman',
+    url='https://github.com/sassoo/goldman',
     license='MIT',
-    download_url = 'https://github.com/sassoo/goldman/tarball/0.1',
-    keywords = ['framework', 'http', 'jsonapi', 'rest', 'web', 'wsgi'],
-    classifiers = [
+    download_url='https://github.com/sassoo/goldman/tarball/0.1',
+    keywords=['framework', 'http', 'jsonapi', 'rest', 'web', 'wsgi'],
+    classifiers=[
         'Intended Audience :: Developers',
         'Intended Audience :: System Administrators',
         'Topic :: Internet :: WWW/HTTP :: WSGI',
@@ -19,7 +21,7 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
     ],
-    install_requires = [
+    install_requires=[
         'blinker',
         'boto',
         'falcon',
