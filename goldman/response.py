b337905bfeaef123fd9a59b4c8510863a881b8df
sassoo <scottmcclanahan2002@gmail.com>
2015-12-31 11:28:25 -0500

    include a Vary header on our responses by default
    The value is currently 'Accept, Prefer' & the comments in the commit
    describe the rational

--- goldman/response.py
+++ goldman/response.py
@@ -17,6 +17,31 @@ class Response(FalconResponse):
         super(Response, self).__init__(*args, **kwargs)
 
         self.serializer = None
+        self._init_default_headers()
+
+    def _init_default_headers(self):
+        """ Initialize the response object with default headers
+
+        A summary of the rational behind adding the headers
+        by default is detailed below.
+
+        Vary Header
+        ~~~~~~~~~~~
+
+        The `Vary` header will let caches know which request
+        headers should be considered when looking up a cached
+        document.
+
+        The `Vary` header should specify `Accept` since our
+        resources commonly support multiple representations
+        of the data (serializers). The representation is
+        determined by the Accept header.
+
+        The `Vary` header should specify `Prefer` according
+        to RFC 7240.
+        """
+
+        self.set_header('Vary', 'Accept, Prefer')
 
     def disable_caching(self):
         """ Add some headers so the client won't cache the response
