31426078735530cdd0ca0d4d36e5998d23c3fffc
sassoo <scottmcclanahan2002@gmail.com>
2016-01-09 21:59:19 -0500

    polish utils/model_helpers

--- goldman/utils/model_helpers.py
+++ goldman/utils/model_helpers.py
@@ -12,17 +12,18 @@ __all__ = ['rtype_to_model']
 
 
 def rtype_to_model(rtype):
-    """ Return a boolean if the string value represents one
+    """ Return a model class object given a string resource type
 
-    :param val: str
-    :return: bool
-    :raise: ValueError
+    :param rtype:
+        string resource type
+    :return:
+        model class object
+    :raise:
+        ValueError
     """
 
     models = goldman.config.MODELS
-
     for model in models:
         if rtype.lower() == model.RTYPE.lower():
             return model
-
     raise ValueError('%s resource type not registered' % rtype)
